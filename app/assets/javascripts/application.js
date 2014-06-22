8edd509ce5cdcfe3a7ba72fa0efdfaaca6f3cf92
Adam Rensel <adamrensel@gmail.com>
2014-06-22 14:36:53 -0400

    Setup javascript files

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,16 +1,13 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
-//
-// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
-// about supported directives.
-//
-//= require jquery
+//= require vendor/jquery
 //= require jquery_ujs
-//= require turbolinks
-//= require_tree .
+//= require vendor/angular
+//= require vendor/codemirror
+//= require vendor/cs_console
+//= require vendor/socket.io
+//= require vendor/underscore
+//=require app/app
+//=require app/route
+//= require_tree ./app/directives
+//= require_tree ./services
+//= require_tree ./controllers
+//= require_tree ./providers
